39dd1720923cd8dce49ffee3925d05a50ff90956
julianasf-91 <62219546+julianasf-91@users.noreply.github.com>
2020-12-07 18:05:46 -0300

    Utilizado laço de repetição for e condicionais if

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const cenario1 = [0, 0, 1, 0, 0, 1, 0];
-const cenario2 = [0, 0, 0, 0, 1, 0];
+const cenario1 = [0, 0, 1, 0, 0, 1, 0]; //4pulos
+const cenario2 = [0, 0, 0, 0, 1, 0]; //3pulos
 
 const jumpingOnClouds = c => {
-
+    let pulos = -1
+    for (let i = 0; i < c.length; i++) {
+        if (c[i] == 0) {
+            pulos += 1
+            if (c[i] == 0 && c[i+1] == 0 && c[i+2] == 0) {
+                pulos -= 1
+                if (c[i+3] == 0) {
+                    pulos += 1
+                    if(c[i+4] == 0) {
+                        pulos -= 1
+                    }
+                }
+            } 
+        }
+    } return pulos
 }
 
 jumpingOnClouds(cenario1)
